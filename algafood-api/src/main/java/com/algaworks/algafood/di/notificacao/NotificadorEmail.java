4fa17bedb1a1bc1cab010d7c32524997c09997ea
Moises Guilherme <moisescomp@gmail.com>
2022-02-02 14:59:19 -0300

    Without interface

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@Component
 public class NotificadorEmail {
-	
+
+	public NotificadorEmail() {
+		System.out.println("1 NotificadorEmail: ");
+	}
 	
 	public void notificar(Cliente cliente, String mensagem) {
 		System.out.printf("Notificando %s através do e-mail %s: %S\n", 
--- algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/service/AtivacaoClienteService.java
@@ -1,12 +1,23 @@
 package com.algaworks.algafood.service;
 
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NotificadorEmail;
 
+@Component
 public class AtivacaoClienteService {
 	
 	private NotificadorEmail notificador;
+
 	
+	public AtivacaoClienteService(NotificadorEmail notificador) {
+		this.notificador = notificador;
+		System.out.println("2 AtivacaoClienteService: " + notificador);
+	}
+
+
+
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 		notificador.notificar(cliente,  "Seu cadastro no sistema está ativo!");		
