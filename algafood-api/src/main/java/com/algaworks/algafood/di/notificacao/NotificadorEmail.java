bb61ea22832a6863c4747e6da63d4fe640fc40c2
Moises Guilherme <moisescomp@gmail.com>
2022-02-04 09:39:51 -0300

    Changing enviroment project config with properties

--- algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-@Profile("prod")
+@Profile("production")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
