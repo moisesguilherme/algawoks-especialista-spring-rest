a769b162bde13c843c49768a6d73992c46ec7f37
Moises Guilherme <moisescomp@gmail.com>
2022-02-14 14:38:43 -0300

    create a dynamic criteria part 1

--- algafood-api/src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infraestructure/repository/RestauranteRepositoryImpl.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.infraestructure.repository;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,9 +9,10 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StringUtils;
 
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepositoryQueries;
@@ -28,11 +29,22 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries{
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class); // from Restaurante
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		var predicates = new ArrayList<Predicate>();
+		
+
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%");
+		
+		Predicate taxaInicialPredicate = builder
+					.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+
+		Predicate taxaFinalPredicate = builder
+					.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
-		
 		return query.getResultList();
-				
 	}
 }
